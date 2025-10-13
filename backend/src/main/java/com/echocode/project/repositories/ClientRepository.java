fe1d425cf39c186f956adc6f23b0fe52c500cd1f
phillipbowles <pbowles@correo.um.edu.uy>
2025-10-13 09:04:36 -0300

    added login and register controlllers

---
+++ backend/src/main/java/com/echocode/project/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.echocode.project.repositories;
+
+import com.echocode.project.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
