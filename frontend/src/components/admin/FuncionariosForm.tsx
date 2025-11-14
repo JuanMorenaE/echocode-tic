ecfddad41f4d377e01fe748ed1d26b91cf314327
Seba <seba.gigirey@gmail.com>
2025-11-14 09:29:52 -0300

    F

--- frontend/src/components/admin/FuncionariosForm.tsx
+++ frontend/src/components/admin/FuncionariosForm.tsx
@@ -27,19 +27,9 @@ export default function FuncionariosForm({
   return (
     <form onSubmit={handleSubmit}>
       <input
-        value={form.full_name ?? ''}
-        onChange={e => setForm(f => ({ ...f, full_name: e.target.value }))}
+        value={form.firstName ?? ''}
+        onChange={e => setForm(f => ({ ...f, firstName: e.target.value }))}
         placeholder="Nombre completo"
-      />
-      <input
-        value={[form.date_of_entry ?? '']}
-        onChange={e => setForm(f => ({ ...f, date_of_entry: e.target.value }))}
-        placeholder="Fecha de ingreso"
-      />
-       <input
-        value={[form.role ?? '']}
-        onChange={e => setForm(f => ({ ...f, role: e.target.value }))}
-        placeholder="Rol"
       />
        <input
         value={[form.email ?? '']}
@@ -47,13 +37,13 @@ export default function FuncionariosForm({
         placeholder="Email"
       />
        <input
-        value={[form.phone_number ?? '']}
-        onChange={e => setForm(f => ({ ...f, phone_number: e.target.value }))}
+        value={[form.phoneNumber ?? '']}
+        onChange={e => setForm(f => ({ ...f, phoneNumber: e.target.value }))}
         placeholder="Número de teléfono"
       />
        <input
-        value={[form.residence ?? '']}
-        onChange={e => setForm(f => ({ ...f, residence: e.target.value }))}
+        value={[form.address ?? '']}
+        onChange={e => setForm(f => ({ ...f, address: e.target.value }))}
         placeholder="Domicilio"
       />
       
